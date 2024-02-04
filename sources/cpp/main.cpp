f588520c5edbc11f3222ca77bca82c6c80a58f47
TheRunuwayDinosaur <93431648+TheRunuwayDinosaur@users.noreply.github.com>
2024-02-04 13:35:48 +0300

    Delete sources/cpp directory

--- sources/cpp/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <QApplication>
-#include "MainView.h"
-
-int main(int argc, char *argv[]){
-    QApplication a(argc, argv);
-    CMainView window;
-    window.show();
-    return a.exec();
-}
